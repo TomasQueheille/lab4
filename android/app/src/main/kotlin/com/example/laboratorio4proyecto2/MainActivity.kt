746700a7f80404478ea0384ffbe6e57ade4e6f64
MGabino <112911128+MGabino@users.noreply.github.com>
2023-12-15 12:25:19 -0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/laboratorio4proyecto2/MainActivity.kt
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+package com.example.laboratorio4proyecto2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
+=======
+package com.example.laboratorio4proyecto2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
+>>>>>>> e69cf61b5795d290caa0b0cbecbced888b6c4822
